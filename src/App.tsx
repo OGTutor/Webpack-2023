6ea1a9f8c6122172ab990d6cc02af60659528fdf
OGTutor <i.send.you.a.letter@gmail.com>
2023-09-19 17:57:20 +0300

    Updated to WEBPACK-2023 & rewritten in TS

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import { Suspense } from 'react';
+import { Link, Route, Routes } from 'react-router-dom';
+import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
+import { MainPageAsync } from './pages/MainPage/MainPage.async';
+import './styles/index.scss';
+import { useTheme } from './theme/useTheme';
+
+const App = () => {
+	const { theme, toggleTheme } = useTheme();
+
+	return (
+		<div className={`app ${theme}`}>
+			<button onClick={toggleTheme}>TOGGLE</button>
+			<Link to={'/'}>Главная</Link>
+			<Link to={'/about'}>О сайте</Link>
+			<Suspense fallback={<div>Loading...</div>}>
+				<Routes>
+					<Route path={'/about'} element={<AboutPageAsync />} />
+					<Route path={'/'} element={<MainPageAsync />} />
+				</Routes>
+			</Suspense>
+		</div>
+	);
+};
+
+export default App;
